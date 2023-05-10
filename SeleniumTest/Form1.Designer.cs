ea0b004244de083bfa4e7fdd8294ebee36c79d46
OS <OS@DESKTOP-0TI4NIN>
2023-05-10 22:27:00 +0700

    Add project files.

---
+++ SeleniumTest/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace SeleniumTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenBrw = new System.Windows.Forms.Button();
+            this.btnOpenBrw2 = new System.Windows.Forms.Button();
+            this.txbURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpenBrw
+            // 
+            this.btnOpenBrw.Location = new System.Drawing.Point(398, 135);
+            this.btnOpenBrw.Name = "btnOpenBrw";
+            this.btnOpenBrw.Size = new System.Drawing.Size(234, 73);
+            this.btnOpenBrw.TabIndex = 0;
+            this.btnOpenBrw.Text = "CLose Browser";
+            this.btnOpenBrw.UseVisualStyleBackColor = true;
+            this.btnOpenBrw.Click += new System.EventHandler(this.btnOpenBrw_Click);
+            // 
+            // btnOpenBrw2
+            // 
+            this.btnOpenBrw2.Location = new System.Drawing.Point(50, 130);
+            this.btnOpenBrw2.Name = "btnOpenBrw2";
+            this.btnOpenBrw2.Size = new System.Drawing.Size(263, 78);
+            this.btnOpenBrw2.TabIndex = 1;
+            this.btnOpenBrw2.Text = "Open Browser 2";
+            this.btnOpenBrw2.UseVisualStyleBackColor = true;
+            this.btnOpenBrw2.Click += new System.EventHandler(this.btnOpenBrw2_Click);
+            // 
+            // txbURL
+            // 
+            this.txbURL.Location = new System.Drawing.Point(108, 80);
+            this.txbURL.Name = "txbURL";
+            this.txbURL.Size = new System.Drawing.Size(566, 20);
+            this.txbURL.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 268);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbURL);
+            this.Controls.Add(this.btnOpenBrw2);
+            this.Controls.Add(this.btnOpenBrw);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenBrw;
+        private System.Windows.Forms.Button btnOpenBrw2;
+        private System.Windows.Forms.TextBox txbURL;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SeleniumTest/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTest
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver driverGLOBAL;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenBrw_Click(object sender, EventArgs e)
+        {
+            // dieu huong trinh duyet
+            driverGLOBAL.Close();
+        }
+
+        private void btnOpenBrw2_Click(object sender, EventArgs e)
+        {
+            ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true;
+            // cach 2
+            IWebDriver driver = new ChromeDriver(chromeDriverService);
+            driverGLOBAL = driver;
+            driver.Url = "https://www.facebook.com/";
+            driver.Navigate();
+            var element = driver.FindElement(By.XPath("//*[@data-testid='open-registration-form-button']"));
+            element.Click();
+
+            Thread.Sleep(2000); // chờ 2 giây cho mở form đăng ký ra ra
+
+            InputDataIntoFormToRegis(driver);
+        }
+
+        /// <summary>
+        /// Đăng ký form tự động
+        /// </summary>
+        /// <param name="driver"></param>
+        private void InputDataIntoFormToRegis(IWebDriver driver)
+        {
+            var fName = driver.FindElement(By.CssSelector("input[name = 'firstname']"));
+            fName.SendKeys("Luong");
+            var sName = driver.FindElement(By.CssSelector("input[name = 'lastname']"));
+            sName.SendKeys("Long");
+            var mobileNumberOrEmailAddress = driver.FindElement(By.CssSelector("input[name = 'reg_email__']"));
+            mobileNumberOrEmailAddress.SendKeys("0787746181");
+            var newPassword = driver.FindElement(By.CssSelector("input[name = 'reg_passwd__']"));
+            newPassword.SendKeys("Obito@20082000");
+            var day = driver.FindElement(By.CssSelector("select[name = 'birthday_day']"));
+            day.FindElement(By.CssSelector("option[value='20']")).Click();
+            var month = driver.FindElement(By.CssSelector("select[name = 'birthday_month']"));
+            month.FindElement(By.CssSelector("option[value='8']")).Click();
+            var year = driver.FindElement(By.CssSelector("select[name = 'birthday_year']"));
+            year.FindElement(By.CssSelector("option[value='2000']")).Click();
+            var gender = driver.FindElement(By.CssSelector("span[data-name = 'gender_wrapper']"));
+            gender.FindElement(By.CssSelector("input[value='2']")).Click();
+        }
+    }
+}
